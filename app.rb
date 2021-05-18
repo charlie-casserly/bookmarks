2b8e04dea84d21d2b0e2bc194c831fa1b2ce4453
charlie-casserly <charliecasserly215@gmail.com>
2021-05-18 14:51:41 +0100

    First commit

---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class MyApp < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+  
+  get '/' do
+    'Bookmark Manager'
+  end
+
+  get '/bookmarks' do
+    @bookmarks = Bookmark.all
+    erb :'bookmarks/index'
+  end 
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
