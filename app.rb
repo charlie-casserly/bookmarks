c338b009a6ddd43eba15a358a3c99a3991d03c3b
charlie-casserly <charliecasserly215@gmail.com>
2021-05-23 16:32:52 +0100

    About to refactor Update Bookmarks

--- app.rb
+++ app.rb
@@ -31,6 +31,17 @@ class MyApp < Sinatra::Base
     redirect '/bookmarks'
   end
 
+  get '/bookmarks/:id/edit' do
+    @bookmark_id = params[:id]
+    erb :'bookmarks/edit'
+  end
+
+  patch '/bookmarks/:id' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec("UPDATE bookmarks SET url = '#{params[:url]}', title = '#{params[:title]}' WHERE id = '#{params[:id]}'")
+    redirect('/bookmarks')
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
